2809a035587d119c497547c5b594504228002182
syed saifi <syed.saifi@oracle.com>
2017-06-22 00:51:08 +0530

    first commit

---
+++ src/com/example/services/PersonService.java
@@ -0,0 +1,25 @@
+package com.example.services;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+import com.example.beans.Person;
+
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface PersonService {
+
+	@WebMethod
+	public boolean addPerson(Person p);
+	
+	@WebMethod
+	public boolean deletePerson(int id);
+	
+	@WebMethod
+	public Person getPerson(int id);
+	
+	@WebMethod
+	public Person[] getAllPersons();
+}
